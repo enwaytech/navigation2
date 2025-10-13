2482d0828243c3314d0bb71237168bae26a40f58
Adi Vardi <adi.vardi@enway.ai>
2025-10-13 14:45:39 +0200

    [tmp] constrain before publish w.r.t to prev u_pub

--- nav2_mppi_controller/src/optimizer.cpp
+++ nav2_mppi_controller/src/optimizer.cpp
@@ -400,8 +400,8 @@ void Optimizer::applyControlSequenceConstraints()
 
   // --tried 1 limit ctrl_seq_(0) based on accel_limit from current robot speed (= state.vx(0,0))  (instead of in predict -> see it still accel issue)
   // TODO 4 or ideally based on last published command
-
   // at this point, control_sequence_ contains the softmax mean of state_.cu (u_virt)]
+/*
   float vx_last = utils::clamp(s.constraints.vx_min, s.constraints.vx_max, control_sequence_.vx(0));
   float wz_last = utils::clamp(-s.constraints.wz, s.constraints.wz, control_sequence_.wz(0));
 
@@ -412,19 +412,20 @@ void Optimizer::applyControlSequenceConstraints()
     vy_last = utils::clamp(-s.constraints.vy, s.constraints.vy, control_sequence_.vy(0));
     control_sequence_.vy(0) = vy_last;
   }
-
-  /*
+*/
   // limit acceleration between current feedback speed and first control in the sequence
-  float vx_last = static_cast<float>(state_.speed.linear.x);
-  float wz_last = static_cast<float>(state_.speed.angular.z);
+  // float vx_last = static_cast<float>(state_.speed.linear.x);
+  // float wz_last = static_cast<float>(state_.speed.angular.z);
+  float vx_last = initial_velocities_(0);
+  float wz_last = initial_velocities_(2);
 
   float vy_last = 0;
   if (isHolonomic()) {
-    vy_last = static_cast<float>(state_.speed.linear.y);
+    // vy_last = static_cast<float>(state_.speed.linear.y);
+    vy_last = initial_velocities_(1);
   }
-  */
 
-  for (unsigned int i = 1; i != control_sequence_.vx.size(); i++) {
+  for (unsigned int i = 0; i != control_sequence_.vx.size(); i++) {
     float & vx_curr = control_sequence_.vx(i);
     vx_curr = utils::clamp(s.constraints.vx_min, s.constraints.vx_max, vx_curr);
     if(vx_last > 0) {
@@ -468,14 +469,14 @@ void Optimizer::applyControlSequenceConstraints()
 }
 
 void Optimizer::updateStateVelocities(
-  models::State & state) const
+  models::State & state)
 {
   updateInitialStateVelocities(state);
   propagateStateVelocitiesFromInitials(state);
 }
 
 void Optimizer::updateInitialStateVelocities(
-  models::State & state) const
+  models::State & state)
 {
   // state.vx.col(0) = static_cast<float>(state.speed.linear.x);
   // state.wz.col(0) = static_cast<float>(state.speed.angular.z);
@@ -486,6 +487,11 @@ void Optimizer::updateInitialStateVelocities(
     // state.vy.col(0) = static_cast<float>(state.speed.linear.y);
     state.vy.col(0) = control_sequence_.vy(0);
   }
+
+  // save for later
+  initial_velocities_(0) = control_sequence_.vx(0);
+  initial_velocities_(1) = control_sequence_.vy(0);
+  initial_velocities_(2) = control_sequence_.wz(0);
 
   // std::cout << "updateInitialStateVelocities: (" << state.speed.linear.x << "  ,  " << state.speed.angular.z << ")\n"
   //  << state.vx(0, Eigen::seq(0, 5)) << "\n " << state.wz(0, Eigen::seq(0, 5)) << std::endl;
